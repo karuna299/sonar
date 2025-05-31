ec74f3766d69ee18c0d826a404acba089d2bfd11
karuna <karunaissa400@gmail.com>
2025-05-31 12:41:51 +0000

    new

---
+++ send_email.py
@@ -0,0 +1,6 @@
+def send_email(recipient, subject, body):
+    password = "SuperSecret123"
+    print("Sending email to", recipient)
+    # (simulated) pretend to send the email
+    print("Subject:", subject)
+    print("Body:", body)
